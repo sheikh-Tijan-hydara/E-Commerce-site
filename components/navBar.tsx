d223e49767749f97da1c7ce556a8fcb9afd508df
TijanAziz <hydarasheikhtijan30@gmail.com>
2024-02-18 18:11:18 +0100

    added logo to home navigation

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -2,13 +2,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping, faUser } from '@fortawesome/free-solid-svg-icons';
 import logo from './../public/images/logo.png'
 import 'tailwindcss/tailwind.css'; 
+import Link from "next/link";
 
 
 export default function NavBar() {
     return(
         
       <div className="flex flex-row justify-between w-full items-center py-4 px-20 "> 
+      <Link href={`/`}>
       <img src={logo.src} alt="logo" className="w-32 h-16" />
+      </Link>
       <div>
         <ul className="flex flex-row space-x-12">
           <li className="font-bold text-slate-800 cursor-pointer hover:bg-gray-200 px-2 py-1 rounded">Home</li>
